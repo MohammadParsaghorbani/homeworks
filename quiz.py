dd6974da5d57785f3270db6fa20f4ff601a31ecf
MohammadParsaghorbani <147430428+MohammadParsaghorbani@users.noreply.github.com>
2024-08-29 12:52:09 +0330

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,2 +1,4 @@
 #hello this is new homework
 #this is quiz!
+#at first we have test
+print("\nwelcom to our quiz!\nscore:\n\t80 - 100 = grate\n\t60 - 80 = good\n\t60 = pass\n\telse you will fail!\n")
