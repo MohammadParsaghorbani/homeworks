173277b606d806d5b2245fe25d122c6c602b3f3b
MohammadParsaghorbani <147430428+MohammadParsaghorbani@users.noreply.github.com>
2024-08-31 19:55:29 +0330

    Update aski_1.py

--- aski_1.py
+++ aski_1.py
@@ -1,7 +1,9 @@
 again = "y"
 while again == "y":
+    result = ""
     char = input("chr : ")
-    key = ord(char) * 2 +5
-    encrypt = chr(key)
-    print(encrypt) 
+    for i in char:
+        z = ord(i)*2+5
+        result += chr(z)
+    print(result) 
     again = input("y/n: ")
