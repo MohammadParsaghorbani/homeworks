e8c04cedb14213825d8ba67b21378f16e5c345f6
MohammadParsaghorbani <mparsagh221@gmail.com>
2024-08-31 21:21:24 +0330

    Update home_work_1.py

--- home_work_1.py
+++ home_work_1.py
@@ -1,16 +1,16 @@
 #encrypt , decrypt
-out_put = ""
-out_put2 = ""
 while True:
     print("your option:\n\t1)encrypt\n\t2)decrypt\n\t3)exit")
     user_choise = int(input("choose on: "))
     if user_choise == 1:
+        out_put = ""
         text = input("enter your text: ")
         for i in text:
             z = ord(i)+11
             out_put += chr(z)
         print(f"out put = \n{out_put}")
     elif user_choise == 2:
+        out_put2 = ""
         way = int(input("we have to way for decrypt,which one? "))
         text = input("enter yor text: ")
         if way == 1:
