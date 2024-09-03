01cd44d6bc2d4d206c4ea51b0354ac162853b5bc
MohammadParsaghorbani <mparsagh221@gmail.com>
2024-09-03 16:40:49 +0330

    Delete aski_1.py

--- aski_1.py
+++
@@ -1,9 +0,0 @@
-again = "y"
-while again == "y":
-    result = ""
-    char = input("chr : ")
-    for i in char:
-        z = ord(i)*2+5
-        result += chr(z)
-    print(result) 
-    again = input("y/n: ")
