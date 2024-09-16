5d32be6d19adabfc5fbc06bf21b99efbe8d04fa8
MohammadParsaghorbani <mparsagh221@gmail.com>
2024-09-16 07:42:34 +0330

    update

--- python_test.py
+++ python_test.py
@@ -7,7 +7,7 @@ name = input("enter your name here: ")
 last_name = input("enter your last name please: ")
 
 with open(f'{name} {last_name}_result.txt' , "a") as file:
-    file.write(f"today = {today.year}/{today.month}/{today.day} - {today.time()}\nname = {name}\nlast name = {last_name}\n")
+    file.write(f"**************************************\ntoday = {today.year}/{today.month}/{today.day} - {today.time()}\nname = {name}\nlast name = {last_name}\n")
 
 while True:
     score = 0
@@ -15,9 +15,16 @@ while True:
         "a = (1,2,3)\nprint(list(a))\n1)[1,2,3]\t2)(1,2,3)\n3)1,2,3 \t4)error" : 1,
         "which one is not str?\n1)'1'\t\t2)str('2')\n3)ord('a') \t4)chr(90)".format(name) :  3,
         "we use ____ for remove item from list:\n1)remove\t2)pop\n3)delete \t4)1 and 2" : 4,
-        "we use ___ to make arrey:\n1)list \t\t2)numpy\n3)tuple \t4)set" : 2
+        "we use ___ to make arrey:\n1)list \t\t2)numpy\n3)tuple \t4)set" : 2,
+        "which option is result of this code?\na = 5\nb = 3\nprint(a<b)\n1)false \t2)correct\n3)wrong \t4)True" : 4,
+        "How to count the letters of a variable or the number of items in a list?\n1)len \t\t2)extend\n3)count \t4)num" : 1,
+        "Which type of Programming does Python support?\n1)object-oriented programming \t2)structured programming\n3)functional programming \t4)all of the mentioned" : 4,
+        "4 + 3 % 5?\n1)7 \t2)2\n3)4 \t4)1" : 1,
+        "Which keyword is used for function in Python language?\n1)function \t2)def\n3)fun \t\t4)define" : 2,
+        "2**(3**2)\n(2**3)**2\n2**3**2\n1)512, 64, 512 \t2)512, 512, 512\n3)64, 512, 64 \t4)64, 64, 64" : 1,
+        "Which of the following is the truncation division operator in Python?\n1)| \t2)//\n3)/ \t4)%" : 2
     }
-    for i in range(4):
+    for i in range(10):
         res = ""
         z = list(quastions.keys())
         x = rnd.choice(z)
@@ -34,7 +41,7 @@ while True:
             file.write(f"answer = {q}\ncorrect answer = {quastions[x]} ")
         file = open(f'{name} {last_name}_result.txt' , "a")
         if q == quastions[x]:
-            score+=25
+            score += 10
             file.write("\ncorrect\n")
         else:
             file.write("\nwrong\n")
